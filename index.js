358a129eb1c6244ffb3c01fcb4c44f167a3bc397
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-03-15 21:56:11 -0700

    Completed Lab; explored different arithmetic operations in JS

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// is an equation whose multiplied results will equal 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+//generates a random integer greater than 0
+const random = (Math.ceil(Math.random()*99));
+
+//is an equation that calculates a remainder that is equal to 4
+const num3 = 25;
+const num4 = 7;
+const mod = num3 % num4;
+
+//will return 20 as the highest number in the set
+const max = Math.max(1,5,8,10,15,18,20);
